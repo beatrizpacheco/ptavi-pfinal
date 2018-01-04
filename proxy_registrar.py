1e48203356b82f4501fe59b29b2b65624dece1bb
bea <b.pacheco@alumnos.urjc.es>
2018-01-04 16:35:23 +0100

    Cambiada la forma de recibir del handle. indicados los pasos con comentarios

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -13,7 +13,7 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
-class proxyHandler(ContentHandler):
+class ProxyHandler(ContentHandler):
 
     config = {}
 
@@ -32,7 +32,7 @@ class proxyHandler(ContentHandler):
         
     def elparser(fich):
         parser = make_parser()
-        prHandler = proxyHandler()
+        prHandler = ProxyHandler()
         parser.setContentHandler(prHandler)
         parser.parse(open(fich))
         return(prHandler.get_tags())
@@ -44,34 +44,21 @@ class ProxyRegisterHandler(socketserver.DatagramRequestHandler):
     """
     dic_users = {}
     """
-    Aquí iría la movida del log y de echar a los clientes expirados#############
-    """
-    """def json2registered(self):
-        """"""
-        method to view clients of database
-        """"""
-        try:
-            with open('registered.json', 'r') as fich:
-                self.dic_users = json.load(fich)
-                self.expired()
-        except (NameError, FileNotFoundError):
-            pass
-    ESTO SERÍA PARA EL OPCIONAL DE LEER DEL FICHERO        
+    Aquí iría la movida del log
     """
 
-    def WriteDatabase(self, path):
+    def write_database(self, path):
         """
         method to save the users in the database
         """
         self.expired()
-        fich = open(path, "w")
-    
-        for user in self.dic_users:
-            line = (user + ', ' + self.dic_users[user][0] + ', ' + 
-                   str(self.dic_users[user][1]) + ', ' +
-                   self.dic_users[user][2] + ', ' + 
-                   self.dic_users[user][3] + '\r\n')
-            fich.write(line)
+        with open(path, "w") as fich:
+            for user in self.dic_users:
+                line = (user + ', ' + self.dic_users[user][0] + ', ' + 
+                       str(self.dic_users[user][1]) + ', ' +
+                       self.dic_users[user][2] + ', ' + 
+                       self.dic_users[user][3] + '\r\n')
+                fich.write(line)
 
     
     def expired(self):
@@ -86,12 +73,7 @@ class ProxyRegisterHandler(socketserver.DatagramRequestHandler):
                 expired_users.append(user)
         for user in expired_users:
             del self.dic_users[user]
-    
-    def user_register(self, fich):
-        """
-        Comprueba si usuario registrado o no
-        """
-        
+
     
     def handle(self):
         """
@@ -99,41 +81,57 @@ class ProxyRegisterHandler(socketserver.DatagramRequestHandler):
         (all requests will be handled by this method)
         """
         #self.json2registered()
-        for line in self.rfile:
-            message = line.decode('utf-8').split()
-            if message and (message[0] == 'REGISTER' or
-                            message[0] == 'register'):
-                print('recibo un register')
-                user = message[1].split(':')[1]
-                ip_address = self.client_address[0]
-                port_address = self.client_address[1]
-            if message and message[0] == 'Expires:':
-                print('linea del expires')
-                if message[1] != '0':
-                    time_regist = time.strftime('%Y-%m-%d %H:%M:%S',
-                                                  time.gmtime(time.time()))
-                    expire =  time.strftime('%Y-%m-%d %H:%M:%S',
-                                            time.gmtime(time.time() +
-                                            int(message[1])))
-                    self.dic_users[user] = [ip_address, port_address,
-                                            time_regist, expire]
-                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                elif message[1] == '0':
-                    try:
-                        del self.dic_users[user]
-                        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                    except KeyError:
-                        self.wfile.write(b'SIP/2.0 404 User '
-                                         b'Not Found\r\n\r\n')
-            if message and (message[0] == 'INVITE' or
-                            message[0] == 'invite'):
-                print('recibo un invite')
-                #voy guardando el invite con sdp en el nuevo mensaje
-                #abro socket con el cliente y le envio el mensaje
-            #print(line.decode('utf-8'), end='')
+        message= self.rfile.read().decode('utf-8')
+        print('lineeeeeeeeee ' + message)#COMPROBACION
+        method = message.split()[0]
+        print('métodooooooo ' + method)#COMPROBACION
+        if (method == 'REGISTER' or method == 'register'):
+            user = message.split()[1].split(':')[1]
+            ip_address = self.client_address[0]
+            port_address = self.client_address[1]
+            print(user + ' ' + ip_address + ' ' + str(port_address))#COMPROBACION
+            if user in self.dic_users:
+                pass
+                #cambio expire
+                #envío 200ok
+            else:
+                if message.split('\r\n')[2]:
+                    if message.split()[5] == 'Authorization:':
+                        pass
+                        #compruebo si el nonce coincide con mi nonce
+                        #if su_nonce==mi_nonce:
+                            #añado usuario al dic y 200ok
+                        #else:
+                            #error mal formado o no autorizado o algo
+                    else:
+                        pass
+                        #envío el nonce
+                
+            
+        elif (method == 'INVITE' or method == 'invite'):
+            pass
+            #miro en mi dic si usuario está
+            #if user in mi dic:
+                #reenvio invite
+            #else
+                #user not found
+        elif (method == 'BYE' or method == 'bye'):
+            pass
+            #miro en mi dic si usuario está
+            #if user in mi dic:
+                #lo borro del dic
+            #else
+                #user not found
+        elif (method == 'ACK' or method == 'ack'):
+            pass
+            #no sé si esta es necesaria
+        else:
+            pass
+            #error mal formado
+            
+            
         print(self.dic_users)
-        self.WriteDatabase(DB_PATH)
-        
+        self.write_database(DB_PATH)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -141,16 +139,16 @@ if __name__ == "__main__":
     CONFIG = sys.argv[1]
     if not os.path.exists(CONFIG):
         sys.exit("Config_file doesn't exists")
-    print(proxyHandler.elparser(CONFIG))
+    print(ProxyHandler.elparser(CONFIG))
     
     #Doy valor a las variables segun la info del xml
-    if proxyHandler.config["server_ip"] == None:
+    if ProxyHandler.config["server_ip"] == None:
         IP_SERVER = "127.0.0.1"
     else:
-        IP_SERVER = proxyHandler.config['server_ip']
-    NAME_SERVER = proxyHandler.config['server_name']
-    PORT_SERVER = int(proxyHandler.config['server_puerto'])
-    DB_PATH = proxyHandler.config['database_path']
+        IP_SERVER = ProxyHandler.config['server_ip']
+    NAME_SERVER = ProxyHandler.config['server_name']
+    PORT_SERVER = int(ProxyHandler.config['server_puerto'])
+    DB_PATH = ProxyHandler.config['database_path']
        
         
     serv = socketserver.UDPServer((IP_SERVER, PORT_SERVER), ProxyRegisterHandler)
